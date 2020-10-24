54f8f66f1812b1121e17fe330dfe3c7f0f918f5a
Leonardo de Oliveira <leonardo.oliveira@velow.com.br>
2020-10-24 17:15:11 -0300

    Finished project

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,11 @@ import errorHandler from './errors/handler'
 const app = express()
 
 app.use(express.json())
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "http://192.168.1.111:3000")
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+   next()
+})
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler)
